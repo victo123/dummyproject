168665f36d2bb574dbdab8893b72c247753a8816
DESKTOP-PN230RF\Victor <onkry.victor@creontek.com>
2022-06-11 17:48:15 +0700

    first commit

---
+++ React-FrontEnd/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Register from './Components/Register';
+import Login from './Components/Login';
+import Home from './Components/Home';
+import Layout from './Components/Layout';
+import Editor from './Components/Editor';
+import Admin from './Components/Admin';
+import Missing from './Components/Missing';
+import Unauthorized from './Components/Unauthorized';
+import Lounge from './Components/Lounge';
+import LinkPage from './Components/LinkPage';
+import RequireAuth from './Components/RequireAuth';
+import {Routes,Route} from 'react-router-dom'
+
+
+function App() {
+ 
+
+  return (
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* public routes */}
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="linkpage" element={<LinkPage />} />
+          <Route path="unauthorized" element={<Unauthorized />} />
+
+          {/* we want to protect these routes */}
+          <Route element={<RequireAuth allowedRoles={"student"} />} >
+        
+            <Route path="/" element={<Home />} />
+          
+            <Route path="editor" element={<Editor />} />
+    
+            <Route path="admin" element={<Admin />} />
+        
+            <Route path="lounge" element={<Lounge />} />
+          </Route>
+
+          {/* catch all */}
+          <Route path="*" element={<Missing />} />
+        </Route>
+    </Routes>
+  );
+}
+export default App;
---
+++ React-FrontEnd/src/Components/Login.js
@@ -0,0 +1,114 @@
+import React from 'react'
+import { useRef,useState,useEffect,useContext } from 'react'
+import useAuth from "../hooks/useAuth"
+import axios from '../api/axios';
+import { Link,useNavigate,useLocation } from 'react-router-dom';
+const LOGIN_URL ='/api/Authentication/login';
+
+
+const Login = () => {
+
+    const {setAuth} = useAuth();
+    
+    const navigate = useNavigate();
+    const location =useLocation();
+  
+    const from = location.state?.from?.pathname || "/";
+
+
+    const userRef=useRef();
+    const errRef=useRef();
+    
+
+    const [user,setUser] = useState('');
+    const [pwd,setPwd] = useState('');
+    const [errMsg,setErrMsg] = useState('');
+ 
+
+
+    useEffect(()=>{
+        userRef.current.focus();
+    },[])
+
+    useEffect(()=> {
+        setErrMsg('');
+    },[user,pwd])
+
+    const handleSubmit= async (e) => 
+    {
+        e.preventDefault();
+        try {
+            const response = await axios.post(LOGIN_URL,
+                JSON.stringify({emailAddress:user,password:pwd}),
+                {
+                    headers:{'Content-Type':'application/json'},
+                    withCredentials:true
+                } 
+            );
+            console.log(JSON.stringify(response?.data));
+            //console.log(JSON.stringify(response));
+            const accessToken = response.data.authResult.token;
+            const userRole = response.data.userRoles;
+            setAuth({ user, pwd, userRole, accessToken });
+            setUser('');
+            setPwd('');
+            //navigate
+            navigate("/", {replace : true});
+        } catch (err) {
+            if (!err?.response) {
+                setErrMsg('');
+            }
+            else if(err.response?.status === 400) {
+                setErrMsg('Missing username or password')
+            }
+            else {
+                setErrMsg(err.response.message)
+            }
+            errRef.current.focus();
+        }
+
+      
+    }
+
+  return (
+      <>
+      
+            <section>
+                <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+                <h1>Sign In</h1>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="useremail">Email Address:</label>
+                    <input
+                        type="text"
+                        id="useremail"
+                        ref={userRef}
+                        autoComplete="off"
+                        onChange={(e) => setUser(e.target.value)}
+                        value={user}
+                        required
+                    />
+
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        onChange={(e) => setPwd(e.target.value)}
+                        value={pwd}
+                        required
+                    />
+                    <button id="reg-btn">Sign In</button>
+                </form>
+                <p>
+                    Need an Account?<br />
+                    <span className="line">                        
+                        <Link to="/register">Sign Up</Link>
+                    </span>
+                </p>
+            </section>
+           
+      </>
+      
+  )
+}
+
+export default Login
